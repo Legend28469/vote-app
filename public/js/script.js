06b142bf4614ac59ec7373a670c3d18625c8424d
Johnathan Brown <johnathaneb@gmail.com>
2017-05-06 06:19:51 -0400

    Charts now generate a random color for each answer

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,26 @@
 // To keep track of answers made in new poll
 var iteration = 3;
 
+// Generate a color for each item in the dataset
+var dynamicColors = function() {
+    var r = Math.floor(Math.random() * 255);
+    var g = Math.floor(Math.random() * 255);
+    var b = Math.floor(Math.random() * 255);
+    return "rgb(" + r + "," + g + "," + b + ")";
+}
+
 $(document).ready(function() {
   var labels = [];
   var dataset = [];
+  var colors = [];
 
   for (var i in data.answers) {
     labels.push(data.answers[i].answer);
     dataset.push(data.answers[i].votes);
+    colors.push(dynamicColors());
   }
+
+  console.log(colors);
 
   var ctx = document.getElementById("chart");
   var myChart = new Chart(ctx, {
@@ -18,7 +30,12 @@ $(document).ready(function() {
           datasets: [{
               label: 'Number of Votes',
               data: dataset,
+              backgroundColor: colors,
+              hoverBackgroundColor: colors
           }]
+      },
+      options: {
+        maintainAspectRatio: true
       }
   });
 });
